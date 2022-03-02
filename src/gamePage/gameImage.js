d26713511f25948386a4e8f6a04d80edeff273f0
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-03-02 12:24:45 -0800

    working on logic for if character is found

--- src/gamePage/gameImage.js
+++ src/gamePage/gameImage.js
@@ -7,10 +7,15 @@ const GameImage = () => {
     const [coordinates, setCoordinates] = useState([]);
 
     const handleImageClick = (e) => {
-        let offsetX = ((e.target.offsetWidth / e.pageX) * 100).toFixed(3);
-        let offsetY = ((e.target.offsetHeight / e.pageY) * 100).toFixed(3);
+        let offsetX = ((e.pageX / e.target.offsetWidth) * 100).toFixed(2);
+        let offsetY = ((e.pageY / e.target.offsetHeight) * 100).toFixed(2);
 
-        console.log(offsetX, offsetY)
+        let targetXCoords = [80.45, 82.99]
+        let targetYCoords = [127.96, 132.47]
+    
+        if((offsetX >= targetXCoords[0] && offsetX <= targetXCoords[1]) && (offsetY >= targetYCoords[0] && offsetY <= targetYCoords[1])) {
+            console.log('found')
+        }
     }
 
     return((
