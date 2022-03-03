e1cfaa0403ce8fb9ca1c9b08bd8be80ad27b05cf
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-03-03 11:51:04 -0800

    started work on homepage

--- src/startUpPage/startup.js
+++ src/startUpPage/startup.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
+import './startup.css';
 
 const StartUpPage = () => {
 
     return(
-        <div>
-            Start Up Page Here 
+        <div id='startUpPageContainer'>
+            <div id='title'>WELCOME TO THE RESISTANCE!</div>
+            <div className="paragraph">
+            On the dessert planet of TATOOINE, some old friends (and foes) were spotted. 
+            You are tasked to find them and recruit them to the RESISTANCE. Keep aware at all times,
+            as we have intel that there are some old IMPERIAL sympathizers on the planet with a mission
+            to take our old friends out!
+            </div>
             <Link to='game'>
-                <button>Go to game</button>
+                <button id='startGameButton'>START MISSION</button>
             </Link>
         </div>
     )
